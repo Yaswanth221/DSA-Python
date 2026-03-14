54b0cda5ffcef092bffd72c879f265babd48b927
D Yaswanth Prasad <ydustakar@gmail.com>
2026-03-14 19:05:49 +0530

    added second largest num in array

--- arrays/arrays.py
+++ arrays/arrays.py
@@ -25,8 +25,36 @@ def small_array(nums):
 
     return total
 
+def array_num(nums, target):
+    count = 0
+
+    for num in nums:
+        if target == num:
+            count = count + 1
+    return count 
+
+def second_largest(nums):
+    largest = nums[0]
+
+    for num in nums:
+        if num > largest:
+            largest = num
+    
+    second = nums[0]
+
+    for num in nums:
+        if num > second and num != largest:
+            second = num
+    
+    return second
+
+
+
+
 nums = [10, 20, 40, 30]
 
 print(sum_array(nums))
 print(great_array(nums))
 print(small_array(nums))
+print(array_num(nums, 10))
+print(second_largest(nums))
