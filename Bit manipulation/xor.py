eebc844846b8e33fcff6e3f5bc684f5fcc636aac
D Yaswanth Prasad <ydustakar@gmail.com>
2026-03-27 00:52:53 +0530

    moved nonzeros in an array

---
+++ Bit manipulation/xor.py
@@ -0,0 +1,8 @@
+nums = [2, 2, 1]
+
+result = 0
+
+for n in nums:
+    result ^= n
+
+print(result)
---
+++ arrays/move-nonzeors.py
@@ -0,0 +1,13 @@
+
+nums = [1, 0, 2, 0, 3]
+
+
+def nonzero(nums):
+    pos =0
+    for i in range(len(nums)):
+        if nums[i] !=0:
+            nums[i], nums[pos] = nums[pos],nums[i]
+            pos += 1
+    return nums
+
+print(nonzero(nums))
